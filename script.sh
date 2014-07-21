f0a8e018e4ae0378811c1dcfde1ba5984bd4c4e5
gnuhub <gnuhub@gmail.com>
2014-07-21 23:35:13 +0800

    修正mkdir手误拼写错误

--- script.sh
+++ script.sh
@@ -17,5 +17,5 @@ sudo ntpdate cn.pool.ntp.org
 # 安装c c++ 开发工具包 
 sudo zypper -n install patterns-openSUSE-devel_C_C++
 # 创建 root ssh目录
-sudo su - root -c 'mdkir ~/.ssh/'
+sudo su - root -c 'mkdir ~/.ssh/'
 sudo su - root -c 'cp -f /vagrant/authorized_keys ~/.ssh/'
