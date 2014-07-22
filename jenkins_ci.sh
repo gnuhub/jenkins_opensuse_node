35728481897eb28d7169fca9d6f49f0e91142ce7
gnuhub <gnuhub@gmail.com>
2014-07-22 15:44:53 +0800

    将代码分支与jenkins build关联

--- jenkins_ci.sh
+++ jenkins_ci.sh
@@ -1,9 +1,18 @@
 #/usr/bin/env bash
 set -x
 cd ~/gnuhub/git/jenkins_opensuse_node
+# 防止本地没有提交代码
+git add .
+git commit -a -m "jenkins backup"
+# 用分支名与jenkins build对应
+git checkout -b build_${BUILD_NUMBER}
+git push origin HEAD
+
 vagrant destroy -f 
 ./ci.sh
 vagrant halt -f
 vagrant package --output jenkins_opensuse_node_${BUILD_NUMBER}.box
 jenkins.cli get-job jenkins_opensuse_node > jenkins_opensuse_node.xml
 vagrant box add jenkins_opensuse_node_${BUILD_NUMBER} jenkins_opensuse_node_${BUILD_NUMBER}.box
+# 回到主开发分支
+git checkout master
