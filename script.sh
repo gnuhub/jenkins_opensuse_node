69e1c3890081a5debeea4c822466383d880cb660
gnuhub <gnuhub@gmail.com>
2014-07-23 00:17:20 +0800

    调整安装顺序

--- script.sh
+++ script.sh
@@ -2,23 +2,14 @@
 set -x
 # 增加kiwi 开发版 软件包源
 #sudo zypper rr openSUSE-13.1-DEV
-#sudo zypper rr Virtualization_Appliances
-sudo zypper addrepo -G http://download.opensuse.org/repositories/Virtualization:Appliances/openSUSE_13.1/Virtualization:Appliances.repo
-sudo zypper mr -p 1 Virtualization_Appliances
-sudo zypper refresh
+sudo zypper rr Virtualization_Appliances
+
 # 修改root密码
 sudo su - root -c 'echo "root:gnuhub" | chpasswd'
 # 安装rsync
 sudo zypper -n install rsync
 # 安装jdk
-sudo zypper -n in /vagrant/jdk-7u65-linux-x64.rpm
-# 安装kiwi
-sudo zypper -n install --from Virtualization_Appliances --force-resolution kiwi
-sudo zypper -n install --from Virtualization_Appliances --force-resolution kiwi-desc-vmxboot 
-sudo zypper -n install --from Virtualization_Appliances --force-resolution kiwi-templates
-sudo zypper -n install --from Virtualization_Appliances --force-resolution kiwi-doc
-
-sudo zypper -n install genisoimage
+sudo zypper -n in /vagrant/jdk-7u65-linux-x64.
 # 安装git
 sudo zypper -n install git
 # 修改时区
@@ -27,6 +18,19 @@ sudo cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 # sudo ntpdate cn.pool.ntp.org
 # 安装c c++ 开发工具包 
 sudo zypper -n install patterns-openSUSE-devel_C_C++
+
+# 安装kiwi
+sudo zypper -n install genisoimage
+sudo zypper addrepo -G http://download.opensuse.org/repositories/Virtualization:Appliances/openSUSE_13.1/Virtualization:Appliances.repo
+sudo zypper mr -p 1 Virtualization_Appliances
+sudo zypper refresh
+sudo zypper -n install --from Virtualization_Appliances --force-resolution kiwi
+sudo zypper -n install --from Virtualization_Appliances --force-resolution kiwi-desc-vmxboot 
+sudo zypper -n install --from Virtualization_Appliances --force-resolution kiwi-templates
+sudo zypper -n install --from Virtualization_Appliances --force-resolution kiwi-doc
+sudo zypper rr Virtualization_Appliances
+
+
 # 创建 root ssh目录
 sudo su - root -c 'mkdir ~/.ssh/'
 sudo su - root -c 'cp -f /vagrant/authorized_keys ~/.ssh/'
